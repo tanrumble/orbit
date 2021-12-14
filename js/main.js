3fe71ad5788456aaf11150dbfa7d5e36a3dd4245
tanrumble <87815363+tanrumble@users.noreply.github.com>
2021-12-14 16:46:38 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -67,6 +67,10 @@ function animate() {
         //orbiting
         velocity = momentum / diagonal;
         mdif = velocity * diagonal - momentum;
+        
+        //fix error
+        velocity = velocity + (-mdif / diagonal);
+        mdif = velocity * diagonal - momentum;
         console.log(mdif);
         sphereMoon.position.y += unitX * velocity;
         sphereMoon.position.x -= unitY * velocity;
